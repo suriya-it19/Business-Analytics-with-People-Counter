55d91f2cc75763ae02113d02fbb0815c26f7d443
suriya-it19 <54205777+suriya-it19@users.noreply.github.com>
2021-02-11 16:55:44 +0530

    centroid

--- mylib/box.py
+++ mylib/box.py
@@ -20,7 +20,7 @@ from mylib.centroidtracker import CentroidTracker
 from mylib.mailer import Mailer
 from mylib.trackableobject import TrackableObject
 
-input_video = config.url3
+input_video = 'videos/example_01.mp4'#config.url3
 
 cost_price_model_path = 'F:/TCS_Inframind/Restaurant/models/cost_price/'
 final_price_model_path = 'F:/TCS_Inframind/Restaurant/models/final_price/'
@@ -65,7 +65,6 @@ def run():
         "sofa", "train", "tvmonitor"]
 
     net = cv2.dnn.readNetFromCaffe(config.PROTOTXT, config.MODEL)
-    print(config.url)
     if not input_video:
         if config.url2:
             print("[INFO] Starting the live stream..")
@@ -87,7 +86,8 @@ def run():
         """
         [INFO] Starting the video..
         """
-        vs = cv2.VideoCapture(input_video)
+        vs = VideoStream(input_video).start()        
+        #vs = cv2.VideoCapture(input_video)
 
     image_placeholder = st.empty()
 
@@ -109,7 +109,7 @@ def run():
         if input_video:
             frame = frame[1] if vs else frame
 
-        if config.url is None and frame is None:
+        if config.url1 or config.url2 is None and frame is None:
             break
 
         frame = imutils.resize(frame, width = 500)
@@ -133,46 +133,20 @@ def run():
 
                 box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
                 (startX, startY, endX, endY) = box.astype("int")     
-                centerCoord = (startX+(endX/2), startY[1]+(endY/2))            
 
                 # display the prediction
                 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
                 label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
-                print("[INFO] {}".format(label))
+                #print("[INFO] {}".format(label))
                 cv2.rectangle(frame, (startX, startY), (endX, endY),
                     COLORS[idx], 2)
                 y = startY - 15 if startY - 15 > 15 else startY + 15
                 cv2.putText(frame, label, (startX, y),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)   
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)               
 
         cv2.line(frame, (0, H // 2), (W, H // 2), (0, 0, 0), 3)
         cv2.putText(frame, "-Prediction border - Entrance-", (10, H - ((i * 20) + 200)),
             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
-
-        if centerCoord > H // 2:
-            totalUp += 1
-        elif centerCoord < H // 2:
-            totalDown += 1
-
-        info = [
-        ("Exit", totalUp),
-        ("Enter", totalDown),
-        #("Status", status),
-        ]
-
-        x = totalUp - totalDown
-
-        info2 = [
-        ("Total people inside", x),
-        ]
-
-        for (i, (k, v)) in enumerate(info):
-            text = "{}: {}".format(k, v)
-            cv2.putText(frame, text, (10, H - ((i * 20) + 20)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 2)
-
-        for (i, (k, v)) in enumerate(info2):
-            text = "{}: {}".format(k, v)
-            cv2.putText(frame, text, (265, H - ((i * 20) + 60)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
 
         image_placeholder.image(frame, channels="BGR")
 
@@ -182,34 +156,15 @@ def run():
             year = 5#randrange(5)
             date = f'{month}/2021'
 
-            if totalUp > 5:
-                pro_price = price.predict([[year, month, week]]) + 5000
-            elif totalUp > 10:
-                pro_price = price.predict([[year, month, week]]) + 10000
-            if totalDown < 5:
-                pro_price = price.predict([[year, month, week]]) - 5000
-            if totalDown < 10:
-                pro_price = price.predict([[year, month, week]]) - 10000
-
-            if totalUp > 5:
-                pro_plates = chettinad_mutton_plates.predict([[year, month, week]]) + 10
-            elif totalUp > 10:
-                pro_plates = chettinad_mutton_plates.predict([[year, month, week]]) + 30
-            if totalDown < 5:
-                pro_plates = chettinad_mutton_plates.predict([[year, month, week]]) - 10
-            if totalDown < 10:
-                pro_plates = chettinad_mutton_plates.predict([[year, month, week]]) - 30
-
-
             df = pd.DataFrame({
                 'Date': f'{date}',
                 'Week': f'{week}',
-                'Price': pro_price,
-                'chettinad_mutton_plates' : pro_plates,
+                'Price': price.predict([[year, month, week]]),
+                'chettinad_mutton_plates' : chettinad_mutton_plates.predict([[year, month, week]]),
                 'chettinad_mutton_rating' : chettinad_mutton_rating.predict([[year, month, week]]),
                 'chettinad_mutton_price' : chettinad_mutton_price.predict([[year, month, week]])
             }, index=[0])
 
             st.table(df)#,width = 10000)
-
+        
         table()
--- mylib/cam.py
+++ mylib/cam.py
@@ -11,6 +11,25 @@ def first(mod):
     """
     # Using People Counter and Machine learning
     """
+    def table():
+            week = 1
+            month = 1
+            year = randrange(5)
+            date = f'{month}/2021'
+
+            df = pd.DataFrame({
+                'Date': f'{date}',
+                'Week': f'{week}',
+                'Price': box.price.predict([[year, month, week]]),
+                'chettinad_mutton_plates' : box.chettinad_mutton_plates.predict([[year, month, week]]),
+                'chettinad_mutton_rating' : box.chettinad_mutton_rating.predict([[year, month, week]]),
+                'chettinad_mutton_price' : box.chettinad_mutton_price.predict([[year, month, week]]),
+                'Discount value' : 'prediction'
+            }, index=[0])
+
+            st.table(df)#,width = 10000)
+
+    table()
     if st.button('Specify URL and Click here'):
         if mod == 'Yolo' or 'yolo' or 'YOLO':
                     box.run()
